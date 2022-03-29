ef24d7088d37f30ec796cc2cdde17c1fa9c36dd0
Sofia M M <98534255+SofiaFasullo@users.noreply.github.com>
2022-03-29 23:17:39 +0000

    part A

--- main.py
+++ main.py
@@ -5,14 +5,27 @@ def seq3np1(n):
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count = 0
     while(n != 1):
-        print(n)
+        #print(n)
+        count += 1
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
+    #print(n)                  # the last print is 1
+    #print(count)
+    return(count)
+    
 
 def main():
-	seq3np1(3)
+  upper_bound = int(input("input an upper range value:"))
+  if upper_bound > 0:
+    for i in range(1,upper_bound+1):
+      count = seq3np1(i)
+      print(count)
+  else:
+    print("You must input a postive number")
+    return()
+
 main()
