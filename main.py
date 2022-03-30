9fcabbaf60d0a4892e6056447feb3d9a051abbee
Sofia M M <98534255+SofiaFasullo@users.noreply.github.com>
2022-03-30 02:23:27 +0000

    part b my way

--- main.py
+++ main.py
@@ -22,10 +22,48 @@ def main():
   upper_bound = int(input("input an upper range value:"))
   if upper_bound > 0:
     for i in range(1,upper_bound+1):
+      print(i)
       count = seq3np1(i)
       print(count)
   else:
     print("You must input a postive number")
     return()
 
-main()
+
+import turtle
+grapher = turtle.Turtle()
+wn = turtle.Screen()
+
+def main2():
+  upper_bound = int(input("input an upper range value:"))
+  list = []
+  if upper_bound > 0:
+    for i in range(1,upper_bound+1):
+      #print(i)
+      count = seq3np1(i)
+      #print(count)
+      list.append(count)
+  else:
+    print("You must input a postive number")
+    return()
+  max_seq3np1 = max(list)
+  print(max_seq3np1)
+  wn.setworldcoordinates(-1,-1,upper_bound+2,max_seq3np1+2)
+  grapher.down()
+  grapher.down()
+  grapher.goto(0,max_seq3np1+2)
+  grapher.up()
+  grapher.goto(0,0)
+  grapher.down()
+  grapher.goto(upper_bound+2, 0)
+  grapher.up()
+  grapher.goto(0,0)
+  grapher.down()
+  grapher.color("green")
+  for i in range(1,upper_bound+1):
+    grapher.goto(i,seq3np1(i))
+
+main2()
+wn.exitonclick()
+
+main2()
